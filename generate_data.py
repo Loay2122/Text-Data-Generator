7068b9ad2d8f6b6e929e3b146a58327f3e8259a2
Loay Hassan <loay.acu2001@gmail.com>
2023-06-02 13:21:12 +0300

    Added the rest of punctuation

--- generate_data.py
+++ generate_data.py
@@ -14,7 +14,7 @@ args=parser.parse_args()
 global gray_back
 kernel=np.ones((2,2),np.uint8)
 kernel2=np.ones((1,1),np.uint8)
-punclist=',.?:;'
+punclist= string.punctuation
 
 #Character sets to choose from.
 smallletters=string.ascii_lowercase
